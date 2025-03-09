df90576e044dd34819ddd4c49201a6ba30d3ee1a
ferematsuzaki3232 <fereple.y.matsuzaki@gmail.com>
2025-03-09 23:27:32 +0900

    kadai_010　更新しました

--- kadai_010/holiday.js
+++ kadai_010/holiday.js
@@ -1,7 +1,6 @@
 const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
 
 // for文の場合
-// for (let i(カウンタ変数) = 数字; 条件式(終わりの数字); i++/--)
 for (let i = 0; i <= 15; i++) {
   console.log(holidays[i]);
 }
@@ -12,9 +11,3 @@ while (num < 16) {
   console.log(holidays[num]);
   num = num + 1
 }
-// 上と同じ※++numとnum++（ループになる）を間違えない
-// let num = 0
-// while (num < 16) {
-//   console.log(holidays[num]);
-//   num = ++num
-// }
