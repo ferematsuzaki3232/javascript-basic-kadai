9ab39308493c0fc5bb788c3b0bcfb4b666d19744
ferematsuzaki3232 <fereple.y.matsuzaki@gmail.com>
2025-03-29 19:10:57 +0900

    kadai_mdn_js　更新しました

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,9 +1,6 @@
-const getFullYear = () =>{
-  console.log(2024);
-}
-const getMonth = () =>{
-  console.log(10);
-}
-const getDate = () =>{
-  console.log(12)
-}
+// 指定した日付を作成
+const date = new Date(2024, 9, 12); // 月は0から始まるので10月は「9」
+
+// 日付をフォーマットして出力
+const formattedDate = `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日`;
+console.log(formattedDate);
